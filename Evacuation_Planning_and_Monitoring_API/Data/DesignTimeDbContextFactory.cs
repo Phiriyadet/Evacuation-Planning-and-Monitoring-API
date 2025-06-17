320defebc753300f755d309b070afcef24827282
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-06-17 10:32:36 +0700

    comment designTimeDb

--- Evacuation_Planning_and_Monitoring_API/Data/DesignTimeDbContextFactory.cs
+++ Evacuation_Planning_and_Monitoring_API/Data/DesignTimeDbContextFactory.cs
@@ -4,29 +4,29 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace Evacuation_Planning_and_Monitoring_API.Data
 {
-    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDBContext>
-    {
-        public ApplicationDBContext CreateDbContext(string[] args)
-        {
-            var configBuilder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables();
+    //public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDBContext>
+    //{
+    //    public ApplicationDBContext CreateDbContext(string[] args)
+    //    {
+    //        var configBuilder = new ConfigurationBuilder()
+    //            .SetBasePath(Directory.GetCurrentDirectory())
+    //            .AddJsonFile("appsettings.json")
+    //            .AddEnvironmentVariables();
 
-            var config = configBuilder.Build();
+    //        var config = configBuilder.Build();
 
-            var keyVaultUri = config["VaultUri"];
-            if (!string.IsNullOrEmpty(keyVaultUri))
-            {
-                configBuilder.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential());
-                config = configBuilder.Build(); // reload with key vault
-            }
+    //        var keyVaultUri = config["VaultUri"];
+    //        if (!string.IsNullOrEmpty(keyVaultUri))
+    //        {
+    //            configBuilder.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential());
+    //            config = configBuilder.Build(); // reload with key vault
+    //        }
 
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
-            var connectionString = config["DatabaseConnection"];
-            optionsBuilder.UseSqlServer(connectionString);
+    //        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
+    //        var connectionString = config["DatabaseConnection"];
+    //        optionsBuilder.UseSqlServer(connectionString);
 
-            return new ApplicationDBContext(optionsBuilder.Options);
-        }
-    }
+    //        return new ApplicationDBContext(optionsBuilder.Options);
+    //    }
+    //}
 }
